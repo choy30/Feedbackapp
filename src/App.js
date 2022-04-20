6cb8eaac7b78a6d08712ae5d075fc2e63a78adce
devYeonjun <alcls0026@gmail.com>
2022-04-20 14:13:07 +1200

    Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react'
 
 function App() {
-const title = 'Blog Post'
-const body = 'This is my blog post'
-const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'}
-]
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    { id: 1, text: 'Comment One' },
+    { id: 2, text: 'Comment Two' },
+    { id: 3, text: 'Comment Three' },
+  ]
+
+  const loading = false
+  const showComments = true
+
+  if (loading) return <h1>Loading...</h1>
+
+  const commentBlock = (<div className='comments'>
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+    ))}
+  </ul>
+</div>)
 
   return (
     <div className='container'>
       <h1>{title}</h1>
       <p>{body}</p>
 
-      <div className='comments'>
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-              {comments.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-              ))}
-          </ul>
-      </div>
+      {showComments && commentBlock}
+
+      
     </div>
   )
 }
