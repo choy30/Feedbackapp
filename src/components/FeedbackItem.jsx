45a63671859cd1d754ec25079af78b6e8cab4656
devYeonjun <alcls0026@gmail.com>
2022-04-21 01:39:43 +1200

    Add Componenets, Props & State

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,24 @@
+import { React } from 'react'
+import {FaTimes} from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedbackItem({ item, handleDelete }) {
+
+
+  return (
+    <Card>
+      <div className='num_display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color='purple'/>
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,25 @@
+import React from 'react' 
+import PropTyes from 'prop-types'
+
+function FeedbackStats({ feedback }) {
+  // Caculate ratings AVG
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating
+    }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTyes = {
+    feedback: PropTyes.array.isRequired,
+}
+
+export default FeedbackStats
